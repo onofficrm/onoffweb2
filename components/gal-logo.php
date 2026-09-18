563e40598105a4e895505f054bb796889cdb2469
onofficrm <wuk2002@naver.com>
2026-09-18 15:38:07 +0800

    Reposition and strengthen the GAL header logo

--- components/gal-logo.php
+++ components/gal-logo.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * GAL CHURCH 공식 로고 (이미지)
- * - 참고: img/logo/logo.png (사용자가 제공한 GAL + CHURCH 로고)
+ * - 참고: img/logo/logo-cropped.png (공식 로고의 흰 여백 제거본)
  * @param string $variant light|dark
  * @param string $size    sm|md|lg
  */
@@ -16,8 +16,8 @@ if (!isset($gal_logo_size)) {
     $gal_logo_size = 'md';
 }
 
-$gal_logo_src = G5_URL.'/img/logo/logo.png';
-if (!is_file(G5_PATH.'/img/logo/logo.png') && is_file(G5_PATH.'/img/logo/logo.svg')) {
+$gal_logo_src = G5_URL.'/img/logo/logo-cropped.png';
+if (!is_file(G5_PATH.'/img/logo/logo-cropped.png') && is_file(G5_PATH.'/img/logo/logo.svg')) {
     $gal_logo_src = G5_URL.'/img/logo/logo.svg';
 }
 
@@ -33,8 +33,8 @@ if ($gal_logo_variant === 'dark') {
     src="<?php echo htmlspecialchars($gal_logo_src, ENT_QUOTES, 'UTF-8'); ?>"
     alt="GAL CHURCH"
     class="gal-brand-logo__img"
-    width="360"
-    height="150"
+    width="776"
+    height="391"
     decoding="async"
   >
 </span>
