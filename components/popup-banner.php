1238ae1ec43ea915d3c14dfd58f8b21c487458e4
onofficrm <wuk2002@naver.com>
2026-09-23 08:36:32 +0800

    Replace sample popup copy with GAL Church welcome message.

--- components/popup-banner.php
+++ components/popup-banner.php
@@ -19,9 +19,9 @@ $cmp_site_name = g5site_cfg('site_name', '샘플 사이트');
         <button type="button" class="cmp-popup__close" aria-label="팝업 닫기">
             <i class="fa fa-times" aria-hidden="true"></i>
         </button>
-        <p class="cmp-popup__badge">이벤트</p>
-        <h2 id="cmpPopupTitle" class="cmp-popup__title"><?php echo htmlspecialchars($cmp_site_name, ENT_QUOTES, 'UTF-8'); ?> 오픈 안내</h2>
-        <p class="cmp-popup__desc">샘플 팝업입니다. 이벤트·공지·<?php echo htmlspecialchars($cmp_consult_label, ENT_QUOTES, 'UTF-8'); ?> 유도 문구를 넣어 사용하세요.</p>
+        <p class="cmp-popup__badge">환영</p>
+        <h2 id="cmpPopupTitle" class="cmp-popup__title"><?php echo htmlspecialchars($cmp_site_name, ENT_QUOTES, 'UTF-8'); ?>에 오신 것을 환영합니다</h2>
+        <p class="cmp-popup__desc">어디에 있든 다시 하나님께 GAL 수 있도록, 온라인으로 예배하고 교제하는 교회입니다. 궁금한 점이 있으시면 카카오톡으로 편하게 문의해 주세요.</p>
         <div class="cmp-popup__actions">
             <button type="button" class="btn btn-primary consult-modal-open cmp-popup__cta" data-target="#cmpConsultModal"><?php echo htmlspecialchars($cmp_consult_label, ENT_QUOTES, 'UTF-8'); ?></button>
             <button type="button" class="btn btn-outline cmp-popup__close-btn">닫기</button>
